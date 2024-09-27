cb4afc62602d1de2e7e8e403e8dfad2dfd050389
Felipe Custodio <felipecustodio@proton.me>
2024-09-27 11:05:31 -0300

    adiciona a classe util

---
+++ src/Utilities.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+final class Utilities
+{
+    public static function formatBytes($bytes, $precision = 2)
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1000));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= pow(1000, $pow);
+        return round($bytes, $precision) . $units[$pow];
+    }
+}
