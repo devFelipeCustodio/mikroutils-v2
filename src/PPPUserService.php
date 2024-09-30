21ab1f8898a48829d60e503795f5cfe008da7d8f
Felipe Custodio <devfelipecustodio@proton.me>
2024-09-30 18:21:10 -0300

    finaliza a função findUserBy() e adiciona testestes

--- src/PPPUserService.php
+++ src/PPPUserService.php
@@ -5,29 +5,26 @@ namespace App;
 final class PPPUserService
 {
 
+    private $filterMap = ["name" => "name", "mac" => "caller-id", "ip" => "address"];
 
     public function __construct(private ZabbixService $zabbix, private GatewayFacade $gateway) {}
 
-    public function findUserByName($query)
+    public function findUserBy($filter, $query)
     {
         $hosts = $this->zabbix->fetchHosts();
 
-        $results = [];
-
         foreach ($hosts as $host) {
-
-            $config = GatewayFacade::createConfig($host["interfaces"]["ip"]);
-            $client = GatewayFacade::createClient($config);
-            $this->gateway = GatewayFacade::connect($client);
+            // $config = GatewayFacade::createConfig($host["interfaces"]["ip"]);
+            // $client = GatewayFacade::createClient($config);
+            // $this->gateway = GatewayFacade::connect($client);
             $users = $this->gateway->getActivePPPUsers();
 
-            $filtered = array_filter($users, function ($user) use (&$query) {
-                return preg_match("/$query/i", $user["name"]);
+            $filtered = array_filter($users, function ($user) use (&$filter, &$query) {
+                $regex = preg_match("/$query/i", $user[$this->filterMap[$filter]]);
+                return $regex === 1;
             });
-
-            array_merge($results, $filtered);
-
         }
-        return $results;
+        return $filtered;
     }
+
 }
--- tests/PPPUserServiceTest.php
+++ tests/PPPUserServiceTest.php
@@ -2,26 +2,76 @@
 
 namespace App\Tests;
 
+use App\GatewayFacade;
 use App\PPPUserService;
 use App\ZabbixService;
 use PHPUnit\Framework\TestCase;
 
 class PPPUserServiceTest extends TestCase
 {
-    public function testaBuscaUsuario(): void
+    private $zabbix;
+    private $gateway;
+    private $PPPUserService;
+
+    protected function setUp(): void
     {
-        $zabbix = $this->createStub(ZabbixService::class);
-        $zabbix->method('fetchHosts')->willReturn([
+        $this->zabbix = $this->createStub(ZabbixService::class);
+        $this->zabbix->method("fetchHosts")->willReturn([
             "result" => [
                 "host" => "MKT-DQX-XRM-RTB-GW-AFINET",
                 "interfaces" => ["ip" => $_SERVER["MK_TEST_IP"]]
             ]
         ]);
-        
-        $gateway = $this->createStub(Gate)
-        $PPPUserService = new PPPUserService($zabbix, );
 
-        $results = $PPPUserService->findUserByName("a");
-        $this->assertNotEmpty($results);
+        $this->gateway = $this->createStub(GatewayFacade::class);
+        $this->gateway->method("getActivePPPUsers")->willReturn([
+            [
+                ".id" => "*80009420",
+                "name" => "wf.josearaujo@afinet.com.br",
+                "service" => "pppoe",
+                "caller-id" => "40:ED:00:FD:FA:F2",
+                "address" => "179.127.194.184",
+                "uptime" => "23h59m44s",
+                "encoding" => "",
+                "session-id" => "0x81909420",
+                "limit-bytes-in" => "0",
+                "limit-bytes-out" => "0",
+                "radius" => true
+            ],
+            [
+                ".id" => "*80009421",
+                "name" => "teste@afinet.com.br",
+                "service" => "pppoe",
+                "caller-id" => "42:ED:00:FD:FA:F2",
+                "address" => "179.127.194.185",
+                "uptime" => "23h59m44s",
+                "encoding" => "",
+                "session-id" => "0x81909420",
+                "limit-bytes-in" => "0",
+                "limit-bytes-out" => "0",
+                "radius" => true
+            ]
+        ]);
+        /** @disregard  */
+        $this->PPPUserService = new PPPUserService($this->zabbix, $this->gateway);
+    }
+
+    public function testaBuscaUsuarioPorNome(): void
+    {
+
+        $results = $this->PPPUserService->findUserBy("name", "test");
+        $this->assertCount(1, $results);
+    }
+
+    public function testaBuscaUsuarioPorMAC(): void
+    {
+        $results = $this->PPPUserService->findUserBy("mac", "40:ED:00:FD:FA:F2");
+        $this->assertCount(1, $results);
+    }
+
+    public function testaBuscaUsuarioPorIP(): void
+    {
+        $results = $this->PPPUserService->findUserBy("ip", "179.127.194.184");
+        $this->assertCount(1, $results);
     }
 }
