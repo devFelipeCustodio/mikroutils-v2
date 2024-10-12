5d8ae7194da42c8e06b964088774b617016eff1a
Felipe Custodio <felipecustodio@proton.me>
2024-10-11 22:36:11 -0300

    corrige o bug da autenticação

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -4,24 +4,22 @@ namespace App\Controller;
 
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AuthenticationController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(Request $request, AuthenticationUtils $authenticationUtils): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->getUser())
+            return $this->redirectToRoute("app_user_search");
         $error = $authenticationUtils->getLastAuthenticationError();
-        $lastName = $authenticationUtils->getLastUsername();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('authentication/login.html.twig', [
-            'controller_name' => 'LoginController',
-            'last_name' => $lastName,
+            'controller_name' => 'AuthenticationController',
+            'last_username' => $lastUsername,
             'error' => $error,
         ]);
     }
