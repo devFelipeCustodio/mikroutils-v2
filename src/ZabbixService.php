89f2e1c0b85ddce6fa707e3d8b2d49e8b2ed101c
Felipe Custodio <felipecustodio@proton.me>
2024-10-11 22:38:22 -0300

    remove verificação desnecessária de env var

--- src/ZabbixService.php
+++ src/ZabbixService.php
@@ -14,11 +14,6 @@ class ZabbixService
     public function __construct(
         private HttpClientInterface $client,
     ) {
-        foreach ($this->envVarsMap as $var) {
-            if ($_SERVER[$var] === null)
-                throw new Exception("[ZAB_CON_ERR] Variável de ambiente '$var' não configurada.");
-        }
-
         $this->url = $_SERVER["ZABBIX_URL"] . "/api_jsonrpc.php";
         $this->token = $_SERVER["ZABBIX_AUTH_TOKEN"];
         $this->groupID = $_SERVER["ZABBIX_GW_GROUPID"];
