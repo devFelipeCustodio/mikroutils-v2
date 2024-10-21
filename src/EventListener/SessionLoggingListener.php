3479622d47208993c35b51b8a41c4f2a4a7ca0c9
Felipe Custodio <felipecustodio@proton.me>
2024-10-21 00:04:47 -0300

    adiciona typehint ao método invoke do SessionLogginListener

--- src/EventListener/SessionLoggingListener.php
+++ src/EventListener/SessionLoggingListener.php
@@ -3,6 +3,7 @@
 namespace App\EventListener;
 
 use App\Entity\Session;
+use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
@@ -10,14 +11,17 @@ use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 class SessionLoggingListener
 {
     private $entityManager;
-    public function __construct(EntityManagerInterface $entityManager){
+    public function __construct(EntityManagerInterface $entityManager)
+    {
         $this->entityManager = $entityManager;
     }
 
     public function __invoke(LoginSuccessEvent $event): void
     {
         $session = new Session();
-        $session->setUserId($event->getUser()->getId());
+        /** @var User $user */
+        $user = $event->getUser();
+        $session->setUserId($user->getId());
         $request = $event->getRequest();
         $session->setUserAgent($request->headers->get("User-Agent"));
         $session->setCreatedAt(new DateTimeImmutable());
