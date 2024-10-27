92323e8ddb65aa5da197b2a4c3fed0284d5f84f5
Felipe Custodio <felipecustodio@proton.me>
2024-10-27 18:16:19 -0300

    permite a adição de campos extras ao formulário de busca

--- src/Form/Type/PPUserSearchFormType.php
+++ src/Form/Type/PPUserSearchFormType.php
@@ -5,18 +5,18 @@ namespace App\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class PPUserSearchType extends AbstractType
+class PPUserSearchFormType extends AbstractType
 {
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             "csrf_protection" => false,
-            "hosts" => []
+            "hosts" => [],
+            "allow_extra_fields" => true
         ]);
 
         $resolver->setAllowedTypes('hosts', 'array');
@@ -24,7 +24,7 @@ class PPUserSearchType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('q', SearchType::class, [
+            ->add('query', SearchType::class, [
                 "label" => false,
                 "attr" => [
                     "placeholder" => "Digite um nome, IP ou MAC de usuário"
