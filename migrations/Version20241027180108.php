692f71681158409ebd199afdd04e07e812c1ead5
Felipe Custodio <felipecustodio@proton.me>
2024-10-27 18:13:35 -0300

    limpeza nas migrations

--- migrations/Version20241027180108.php
+++ migrations/Version20241027180108.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241025211230 extends AbstractMigration
+final class Version20241027180108 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,7 @@ final class Version20241025211230 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE search_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE search (id INT NOT NULL, q VARCHAR(255) NOT NULL, user_id INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, hosts JSON NOT NULL, type VARCHAR(255) NOT NULL, page INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE search (id INT NOT NULL, query VARCHAR(255) NOT NULL, user_id INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, hosts JSON NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN search.created_at IS \'(DC2Type:datetime_immutable)\'');
     }
 
@@ -29,7 +28,6 @@ final class Version20241025211230 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE search_id_seq CASCADE');
         $this->addSql('DROP TABLE search');
     }
 }
