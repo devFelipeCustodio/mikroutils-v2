d7b8f620392fa9d4480e7bfcdc02ac83f7632ec4
Felipe Custodio <felipecustodio@proton.me>
2024-11-07 11:49:15 -0300

    corrige o nome do controller PPPUser

--- src/Form/Type/PPPUserSearchFormType.php
+++ src/Form/Type/PPPUserSearchFormType.php
@@ -8,37 +8,38 @@ use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class PPUserSearchFormType extends AbstractType
+class PPPUserSearchFormType extends AbstractType
 {
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            "csrf_protection" => false,
-            "hosts" => [],
-            "allow_extra_fields" => true
+            'csrf_protection' => false,
+            'hosts' => [],
+            'allow_extra_fields' => true,
         ]);
 
         $resolver->setAllowedTypes('hosts', 'array');
     }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('query', SearchType::class, [
-                "label" => false,
-                "attr" => [
-                    "placeholder" => "Digite um nome, IP ou MAC de usuário"
-                ]
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Digite um nome, IP ou MAC de usuário',
+                ],
             ])
             ->add('hosts', ChoiceType::class, [
-                "label" => false,
-                "multiple" => true,
-                "expanded" => true,
+                'label' => false,
+                'multiple' => true,
+                'expanded' => true,
                 'label_attr' => [
-                    'class' => 'checkbox-switch'
+                    'class' => 'checkbox-switch',
                 ],
-                "choices" => array_merge(["Todos" => "all"], $options["hosts"])
+                'choices' => array_merge(['Todos' => 'all'], $options['hosts']),
             ])
         ;
     }
 }
+
