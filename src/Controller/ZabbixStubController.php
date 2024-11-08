06ca40ecd4092743f62dbe5aa84695e2aad853e4
Felipe Custodio <felipecustodio@proton.me>
2024-11-08 11:34:55 -0300

    adiciona mais dados a resposta do mock do zabbix

--- src/Controller/ZabbixStubController.php
+++ src/Controller/ZabbixStubController.php
@@ -9,27 +9,77 @@ use Symfony\Component\Routing\Attribute\Route;
 class ZabbixStubController extends AbstractController
 {
     #[Route('/zabbix', name: 'app_zabbix_stub',
-        methods: ["POST"], condition: "'dev' === '%kernel.environment%'")]
+        methods: ['POST'], condition: "'dev' === '%kernel.environment%'")]
     public function index(): JsonResponse
     {
         return $this->json(
             [
-                "result" => [
+                'result' => [
                     [
-                        "hostid" => "11131",
-                        "host" => "GW-VIRTUAL-1",
-                        "interfaces" => [
-                            ["ip" => "192.168.1.100"]
-                        ]
+                        'hostid' => '11131',
+                        'host' => 'GW-VIRTUAL-1',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.100'],
+                        ],
                     ],
                     [
-                        "hostid" => "11132",
-                        "host" => "GW-VIRTUAL-2",
-                        "interfaces" => [
-                            ["ip" => "192.168.1.101"]
-                        ]
-                    ]
-                ]
+                        'hostid' => '11132',
+                        'host' => 'GW-VIRTUAL-2',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.101'],
+                        ],
+                    ],
+
+                    [
+                        'hostid' => '11133',
+                        'host' => 'GW-VIRTUAL-3',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.102'],
+                        ],
+                    ],
+                    [
+                        'hostid' => '11134',
+                        'host' => 'GW-VIRTUAL-4',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.103'],
+                        ],
+                    ],
+                    [
+                        'hostid' => '11135',
+                        'host' => 'GW-VIRTUAL-5',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.104'],
+                        ],
+                    ],
+                    [
+                        'hostid' => '11136',
+                        'host' => 'GW-VIRTUAL-6',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.105'],
+                        ],
+                    ],
+                    [
+                        'hostid' => '11137',
+                        'host' => 'GW-VIRTUAL-7',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.106'],
+                        ],
+                    ],
+                    [
+                        'hostid' => '11138',
+                        'host' => 'GW-VIRTUAL-8',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.107'],
+                        ],
+                    ],
+                    [
+                        'hostid' => '11139',
+                        'host' => 'GW-VIRTUAL-9',
+                        'interfaces' => [
+                            ['ip' => '192.168.1.108'],
+                        ],
+                    ],
+                ],
             ]
         );
     }
