239ed922710efe2576757494b95d77b04b38cb76
Felipe Custodio <felipecustodio@proton.me>
2024-11-12 01:37:19 -0300

    nova migration

--- migrations/Version20241112033127.php
+++ migrations/Version20241112033127.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241108184555 extends AbstractMigration
+final class Version20241112033127 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,10 @@ final class Version20241108184555 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE export_users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE log_search_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE export_users (id INT NOT NULL, user_id INT NOT NULL, hosts JSON NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN export_users.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE log_search (id INT NOT NULL, user_id INT NOT NULL, query VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, hosts JSON NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN log_search.created_at IS \'(DC2Type:datetime_immutable)\'');
     }
@@ -29,7 +32,9 @@ final class Version20241108184555 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE export_users_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE log_search_id_seq CASCADE');
+        $this->addSql('DROP TABLE export_users');
         $this->addSql('DROP TABLE log_search');
     }
 }
