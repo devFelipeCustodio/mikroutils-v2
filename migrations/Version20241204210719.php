d8af8845f42da6a27884885025e6e08540442f59
Felipe Custodio <devfelipecustodio@proton.me>
2024-12-04 18:49:34 -0300

    limpeza de migrations

--- migrations/Version20241204210719.php
+++ migrations/Version20241204210719.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241129161003 extends AbstractMigration
+final class Version20241204210719 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -42,12 +42,14 @@ final class Version20241129161003 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN "user".created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN "user".allowed_host_ids IS \'(DC2Type:simple_array)\'');
         $this->addSql('CREATE TABLE user_session (id INT NOT NULL, user_id INT NOT NULL, user_agent VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, ip VARCHAR(255) NOT NULL, session_id VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_8849CBDEA76ED395 ON user_session (user_id)');
         $this->addSql('COMMENT ON COLUMN user_session.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE sessions (sess_id VARCHAR(128) NOT NULL, sess_data BYTEA NOT NULL, sess_lifetime INT NOT NULL, sess_time INT NOT NULL, PRIMARY KEY(sess_id))');
         $this->addSql('CREATE INDEX sess_lifetime_idx ON sessions (sess_lifetime)');
         $this->addSql('ALTER TABLE client_export ADD CONSTRAINT FK_172A5D27A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE client_search ADD CONSTRAINT FK_E1569014A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE log_search ADD CONSTRAINT FK_A10A8805A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE user_session ADD CONSTRAINT FK_8849CBDEA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
@@ -62,6 +64,7 @@ final class Version20241129161003 extends AbstractMigration
         $this->addSql('ALTER TABLE client_export DROP CONSTRAINT FK_172A5D27A76ED395');
         $this->addSql('ALTER TABLE client_search DROP CONSTRAINT FK_E1569014A76ED395');
         $this->addSql('ALTER TABLE log_search DROP CONSTRAINT FK_A10A8805A76ED395');
+        $this->addSql('ALTER TABLE user_session DROP CONSTRAINT FK_8849CBDEA76ED395');
         $this->addSql('DROP TABLE client_export');
         $this->addSql('DROP TABLE client_search');
         $this->addSql('DROP TABLE log_search');
