8b833859a51dc0b7253902d1d900c8aab10f346d
Felipe Custodio <felipelinscustodio@proton.me>
2024-12-12 18:33:42 -0300

    adiciona entidade para registro de visualizações de clientes

---
+++ src/Entity/ClientDetail.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientDetailRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientDetailRepository::class)]
+class ClientDetail
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'clientDetails')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user_id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?int $host = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $clientName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getHost(): ?int
+    {
+        return $this->host;
+    }
+
+    public function setHost(int $host): static
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+
+    public function getClientName(): ?string
+    {
+        return $this->clientName;
+    }
+
+    public function setClientName(string $clientName): static
+    {
+        $this->clientName = $clientName;
+
+        return $this;
+    }
+}
